c1381a934890b4940e504fafcbeb2bdd9d5a7195
Mohamed-mekawy <mekawy2012@gmail.com>
2015-09-17 15:02:58 -0700

    3.2 Open Location in map applications uses "geo" Scheme

--- app/src/main/java/com/example/mekawy/sunrise/MainActivity.java
+++ app/src/main/java/com/example/mekawy/sunrise/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.mekawy.sunrise;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.provider.AlarmClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,8 +22,6 @@ public class MainActivity extends ActionBarActivity {
         FetchWeatherTask f=new FetchWeatherTask();
         f.execute("94043");
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,7 +43,24 @@ public class MainActivity extends ActionBarActivity {
             startActivity(new Intent(getApplicationContext(),Setting_Activity.class));
             return true;
         }
-
+        else if(id== R.id.Map_setting){
+            openPreferredLocationInMap();
+        }
         return super.onOptionsItemSelected(item);
     }
+
+    //open Location form preferences in Map application which scheme is "geo"
+    public void openPreferredLocationInMap(){
+        SharedPreferences shrd_pref= PreferenceManager.getDefaultSharedPreferences(this);
+        String current_location=shrd_pref.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        Uri geo_uri=Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q",current_location).build();
+        Intent geo_intent=new Intent(Intent.ACTION_VIEW);
+        geo_intent.setData(geo_uri);
+
+        if(geo_intent.resolveActivity(getPackageManager())!=null)
+            startActivity(geo_intent);
+        else Toast.makeText(getApplicationContext(),"No Such Map Application",Toast.LENGTH_SHORT).show();
+    }
+
+
 }
