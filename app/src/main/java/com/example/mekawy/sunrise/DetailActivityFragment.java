d8fae740d114d5ce9c5515d3077d8ecf4d530e51
Mohamed-mekawy <mekawy2012@gmail.com>
2015-09-18 04:12:21 -0700

    3.5 Add Share ActionProvider Handled in DeatiledActivity

--- app/src/main/java/com/example/mekawy/sunrise/DetailActivityFragment.java
+++ app/src/main/java/com/example/mekawy/sunrise/DetailActivityFragment.java
@@ -3,10 +3,22 @@ package com.example.mekawy.sunrise;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+
+
+import android.support.v4.view.MenuCompat;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -15,9 +27,29 @@ import android.widget.TextView;
 public class DetailActivityFragment extends Fragment {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunriseApp";
+    private String Sharable_Forecast;
+    private String LOG=DetailActivityFragment.class.getSimpleName();
 
     public DetailActivityFragment() {
+    setHasOptionsMenu(true);
+    }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment,menu);
+        MenuItem mItem=menu.findItem(R.id.share);
+         ShareActionProvider mShare
+                =(ShareActionProvider) MenuItemCompat.getActionProvider(mItem);
+        if(mShare!=null)mShare.setShareIntent(createShareForecastIntent());
+        else Log.i(LOG,"NO ACTION.SEND Founded");
+    }
+
+    private Intent createShareForecastIntent(){
+        Intent share_intent=new Intent(Intent.ACTION_SEND);
+        share_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        share_intent.setType("text/plain");
+        share_intent.putExtra(Intent.EXTRA_TEXT,Sharable_Forecast);
+        return share_intent;
     }
 
     @Override
@@ -33,7 +65,8 @@ public class DetailActivityFragment extends Fragment {
         Intent text_intent=getActivity().getIntent();
         if(text_intent!=null && text_intent.hasExtra(Intent.EXTRA_TEXT)){
             TextView forecast_tv=(TextView) root.findViewById(R.id.detail_text);
-            forecast_tv.setText(text_intent.getStringExtra(Intent.EXTRA_TEXT)+FORECAST_SHARE_HASHTAG);
+            Sharable_Forecast=text_intent.getStringExtra(Intent.EXTRA_TEXT)+FORECAST_SHARE_HASHTAG;
+            forecast_tv.setText(Sharable_Forecast);
         }
         return root;
     }
